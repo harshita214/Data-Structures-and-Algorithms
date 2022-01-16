cbf505402d700d693ed15fbdcd3da552aaf11d1b
Harshita sharma <79797000+harshita214@users.noreply.github.com>
2022-01-16 20:45:17 +0530

    Create exception.cpp

---
+++ Strings/exception.cpp
@@ -0,0 +1,18 @@
+// problem link: https://www.hackerrank.com/challenges/30-linked-list?isFullScreen=true
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    try {
+        cout << stoi(s) << endl;
+    } catch(...) {
+        cout << "Bad String" << endl;
+    }
+    
+    return 0;
+}
