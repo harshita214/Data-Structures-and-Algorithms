6584eca3e09219a8b5140bb0f95688b6ec33110d
Harshita sharma <79797000+harshita214@users.noreply.github.com>
2022-01-19 22:32:09 +0530

    Create richestcustomerwealth.cpp

---
+++ Array/richestcustomerwealth.cpp
@@ -0,0 +1,18 @@
+// problem link: https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int wealth = INT_MIN;
+        for(int i=0;i<accounts.size();i++){
+            int sum = 0;
+            for(int j=0;j<accounts[i].size();j++){
+                sum = sum + accounts[i][j];
+                if(sum>wealth){
+                    wealth = sum;
+                }
+             }
+         }
+         return wealth;
+     }
+};
