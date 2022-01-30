addabc95e7cdd7cab5d2c5a8b314f09435cce355
Harshita sharma <79797000+harshita214@users.noreply.github.com>
2022-01-30 21:13:13 +0530

    Update rotatearray.cpp

--- Array/rotatearray.cpp
+++ Array/rotatearray.cpp
@@ -17,3 +17,17 @@ public:
     }
 };
 */
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+	
+        k = k % nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
