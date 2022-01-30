f20b1337187e8885f49285fd349f3e5474cc1736
Harshita sharma <79797000+harshita214@users.noreply.github.com>
2022-01-30 21:11:29 +0530

    Create rotatearray1.cpp

---
+++ Array/rotatearray1.cpp
@@ -0,0 +1,19 @@
+// problem link: 
+
+//First approch
+//Time Complexity : O(n^2)
+//Space Complexity : O(1)
+/*
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+         for(int i=0; i < k; i++) {
+             int temp = nums[nums.size()-1];
+             for(int j=nums.size()-1; j > 0; j--) {
+                 nums[j] = nums[j-1];
+             }
+             nums[0] = temp;
+        }
+    }
+};
+*/
